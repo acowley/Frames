ba333028ef69caf57c78b4813d9b08de3ef8572a
Anthony Cowley <acowley@gmail.com>
2015-01-06 14:04:59 -0500

    Tweak to UserCol ColumnTypeable instance.
    I think this is more suggestive of how to extend the column universe.

--- demo/Tutorial.hs
+++ demo/Tutorial.hs
@@ -259,12 +259,14 @@ femaleOccupations = P.filter ((== Female) . view u2gender)
 
 -- * Appendix: User Types
 
--- Here are the definitions needed to define the ~UserCol~ type with
--- its more descriptive ~GenderT~ type. We have to define these things
--- in a separate module from our main work due to GHC's stage
--- restrictions regarding Template Haskell. Specifically, ~UserCol~
--- and its instances are used at compile time to infer the record type
--- needed to represent the data file.
+-- Here are the definitions needed to define the ~UserCol~ type with its
+-- more descriptive ~GenderT~ type. We have to define these things in a
+-- separate module from our main work due to GHC's stage restrictions
+-- regarding Template Haskell. Specifically, ~UserCol~ and its instances
+-- are used at compile time to infer the record type needed to represent
+-- the data file. Notice the extension point here, though somewhat
+-- buried, is not too rough: you prepend new, more refined, type
+-- compatibility checks to the end of the ~inferType~ definition.
 
 -- This is clearly a bit of a mouthful, and probably not something you'd
 -- want to do for every data set. However, the /ability/ to refine the
@@ -280,6 +282,7 @@ femaleOccupations = P.filter ((== Female) . view u2gender)
 -- import Data.Monoid
 -- import Data.Readable (Readable(fromText))
 -- import qualified Data.Text as T
+-- import Data.Traversable (sequenceA)
 -- import Frames.CSV (ColumnTypeable(..))
 
 -- data GenderT = Male | Female deriving (Eq,Ord,Show)
@@ -303,7 +306,7 @@ femaleOccupations = P.filter ((== Female) . view u2gender)
 --   colType TText = [t|T.Text|]
 --   inferType = let isInt = fmap (const TInt :: Int -> UserCol) . fromText
 --                   isGen = bool Nothing (Just TGender) . (`elem` ["M","F"])
---               in \t -> fromMaybe TText $ isInt t <> isGen t
+--               in fromMaybe TText . mconcat . sequenceA [isGen, isInt]
 -- #+end_src
 
 -- #+DATE:
--- demo/TutorialUsers.hs
+++ demo/TutorialUsers.hs
@@ -6,6 +6,7 @@ import Data.Maybe (fromMaybe)
 import Data.Monoid
 import Data.Readable (Readable(fromText))
 import qualified Data.Text as T
+import Data.Traversable (sequenceA)
 import Frames.CSV (ColumnTypeable(..))
 
 data GenderT = Male | Female deriving (Eq,Ord,Show)
@@ -29,4 +30,4 @@ instance ColumnTypeable UserCol where
   colType TText = [t|T.Text|]
   inferType = let isInt = fmap (const TInt :: Int -> UserCol) . fromText
                   isGen = bool Nothing (Just TGender) . (`elem` ["M","F"])
-              in \t -> fromMaybe TText $ isInt t <> isGen t
+              in fromMaybe TText . mconcat . sequenceA [isGen, isInt]
