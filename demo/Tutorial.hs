0cfde7ba6683dcce0af27d2aeb50ba842431157f
Anthony Cowley <acowley@gmail.com>
2015-01-23 22:54:42 -0500

    Removed last GenderT references.

--- demo/Tutorial.hs
+++ demo/Tutorial.hs
@@ -384,14 +384,15 @@ neOccupations = P.filter (isNewEngland . view u2zipCode)
 -- * Appendix
 -- ** User Types
 
--- Here are the definitions needed to define the ~MyColumns~ type with its
--- more descriptive ~GenderT~ type. We have to define these things in a
--- separate module from our main work due to GHC's stage restrictions
--- regarding Template Haskell. Specifically, ~GenderT~ and its instances
--- are used at compile time to infer the record type needed to represent
--- the data file. Notice the extension point here is not too rough: you
--- prepend new, more refined, type compatibility checks to the head of
--- ~CommonColumns~, or you can build up your own list of expected types.
+-- Here are the definitions needed to define the ~MyColumns~ type with
+-- its more descriptive ~ZipT~ type. We have to define these things in
+-- a separate module from our main work due to GHC's stage
+-- restrictions regarding Template Haskell. Specifically, ~ZipT~ and
+-- its instances are used at compile time to infer the record type
+-- needed to represent the data file. Notice the extension point here
+-- is not too rough: you prepend new, more refined, type compatibility
+-- checks to the head of ~CommonColumns~, or you can build up your own
+-- list of expected types.
 
 -- This may not be something you'd want to do for every data
 -- set. However, the /ability/ to refine the structure of parsed data
