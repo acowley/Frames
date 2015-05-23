a5d2dbc5d9517bd26f4ec15d5a0f0d0daffa446f
Anthony Cowley <acowley@gmail.com>
2015-05-22 20:41:06 -0400

    Demonstrate melting a whole Frame.

--- demo/Melt.hs
+++ demo/Melt.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE DataKinds, QuasiQuotes, TypeOperators #-}
 module Melt where
 import Control.Applicative
+import Data.Foldable (toList)
 import Frames
 
 type Age = "age" :-> Int
 type Weight = "weight" :-> Double
 type Name = "name" :-> String
 
-testRec :: Record [Name, Age, Weight]
-testRec = frameCons (pure "bob")
-        $ frameCons (pure 23)
-        $ frameCons (pure 75.2) Nil
+testRec1 :: Record '[Name, Age, Weight]
+testRec1 = frameCons (pure "bob")
+         $ frameCons (pure 23)
+         $ frameCons (pure 75.2) Nil
 
-testMelt :: [Record '[Name, "value" :-> Field '[Age, Weight]]]
-testMelt = melt [pr1|Name|] testRec
+testMelt :: Record '[Name, Age, Weight]
+         -> [Record '[Name, "value" :-> Field '[Age, Weight]]]
+testMelt = meltRow [pr1|Name|]
 
 type Val = "value" :-> Field '[Age, Weight]
 
@@ -22,4 +24,12 @@ type Val = "value" :-> Field '[Age, Weight]
 testField :: [Record '[Name, "value" :-> Field '[Age, Weight]]]
 testField = filter
               (onField [pr|Ord,Num|] (> 50) . rget (rlens [pr|Val|]))
-              testMelt
+              (testMelt testRec1)
+
+testRec2 :: Record '[Name, Age, Weight]
+testRec2 = frameCons (pure "alice")
+         $ frameCons (pure 34)
+         $ frameCons (pure 55.2) Nil
+
+molten :: [Record '[Name, "value" :-> Field '[Age, Weight]]]
+molten = toList (melt [pr1|Name|] (toFrame [testRec1, testRec2]))
