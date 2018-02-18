381956699540b9b72b92e8a6e9e98a789f3c980d
cfhammill <cfhammill@gmail.com>
2018-02-18 18:27:31 -0500

    Add outer join
    A bit painful that it returns a list of records now in
    the Maybe interpretation functor. I think ultimately I'd
    like them to still be Records with the internal types wrapped
    in Maybe...

--- src/Frames/Joins.hs
+++ src/Frames/Joins.hs
@@ -76,10 +76,49 @@ inner_join cols a b =
       mergeFun l r = mergeRec cols l r
       proj1 x = rcast x :: Record fs
       proj2 y = rcast y :: Record fs
+
+
 justsFromRec :: Record fs -> Rec Maybe fs
 justsFromRec rs = rmap (\x -> Just (getIdentity x)) rs
 
 nothingsFromRec :: Record fs -> Rec Maybe fs
 nothingsFromRec rs = rmap (\_ -> Nothing) rs
+
+-- | Perform an outer join operation on two frames
+-- matching on a quasiquoter of column names
+outer_join :: forall proxy fs rs rs2  rs2'.
+  (fs    ⊆ rs
+    , fs   ⊆ rs2
+    , rs ⊆ (rs ++ rs2')
+    , rs2' ⊆ rs2 
+    , rs2' ~ RDeleteAll fs rs2
+    , Grouping (Record fs)
+    , RecVec rs
+    , RecVec rs2'
+    , RecVec (rs ++ rs2')
+    ) =>
+    proxy fs -> -- ^ A quasiquoter with shared columns to join on,
+                -- usually generated with pr or pr1
+    Frame (Record rs) -> -- ^ The left frame 
+    Frame (Record rs2) -> -- ^ The right frame
+    [(Rec Maybe (rs ++ rs2'))] -- ^ A list of the merged records, now in the Maybe functor
+    
+outer_join cols a b =
+  let
+    as = toList a
+    bs = toList b
+  in
+    let
+      proj1 x = rcast x :: Record fs
+      proj2 y = rcast y :: Record fs
+      mergeFun l r = justsFromRec $ mergeRec cols l r
+      mergeLeftEmpty l = (justsFromRec l) <+> (nothingsFromRec (dropCols cols (head bs)))
+      mergeRightEmpty r = (nothingsFromRec (head as)) <+> (justsFromRec (dropCols cols r))
+    in  
+      foldr' (++) [] 
+      (outer grouping mergeFun mergeLeftEmpty mergeRightEmpty
+       proj1 proj2 (toList a) (toList b))
+
+    
     
         
