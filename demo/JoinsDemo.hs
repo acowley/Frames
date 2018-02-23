de01587b82fcfed7fb8b30bee98ee38c41e0d016
cfhammill <cfhammill@gmail.com>
2018-02-22 20:27:15 -0500

    Elaborate the joins demo

--- demo/JoinsDemo.hs
+++ demo/JoinsDemo.hs
@@ -3,13 +3,12 @@
              FlexibleContexts,
              TemplateHaskell #-}
 
-import Criterion.Main
 import Frames
 import Frames.Joins
-import Data.Foldable as F
 
 tableTypes "LCols" "data/left1.csv"
 tableTypes "RCols" "data/right1.csv"
+tableTypes "SmCols" "data/left_summary.csv"
 
 lfi :: IO (Frame LCols)
 lfi = inCoreAoS (readTable "data/left1.csv")
@@ -17,15 +16,21 @@ lfi = inCoreAoS (readTable "data/left1.csv")
 rfi :: IO (Frame RCols)
 rfi = inCoreAoS (readTable "data/right1.csv")
 
+smfi :: IO (Frame SmCols)
+smfi = inCoreAoS (readTable "data/left_summary.csv")
+
 main :: IO ()
 main = do
   lf <- lfi
   rf <- rfi
-  let rf2 = toFrame $ reverse $ toList rf in
-    let x = innerJoin [pr1|PolicyID|] lf rf 
-        y = innerJoin [pr1|PolicyID|] lf rf2 in
-      print (x == y)
+  smf <- smfi
+  print $ length $ innerJoin [pr1|PolicyID|] lf rf
+  print $ length $ innerJoin [pr1|PolicyID|] lf smf
+  print $ length $ innerJoin [pr|PolicyID,County|] lf smf
+  print $ length $ outerJoin [pr|PolicyID,County|] lf smf
+  print $ length $ leftJoin  [pr|PolicyID,County|] lf smf
+  print $ length $ rightJoin [pr|PolicyID,County|] lf smf                             
+
+    
 
   
-  
-
