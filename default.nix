9848cc25c2f67e354642d343744ebdd077aaaf1f
Anthony Cowley <acowley@gmail.com>
2018-03-15 16:33:58 -0400

    Add a default.nix
    It's not exactly what comes out of cabal2nix on its own. In
    particular, the use of -fllvm for benchmarking means we require llvm.

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ mkDerivation, base, contravariant, criterion, deepseq, directory
+, discrimination, ghc-prim, hashable, hspec, htoml, HUnit, pipes
+, pipes-bytestring, pipes-group, pipes-parse, pipes-safe
+, pipes-text, pretty, primitive, readable, regex-applicative
+, stdenv, template-haskell, temporary, text, transformers
+, unordered-containers, vector, vinyl
+, Chart, Chart-diagrams, diagrams-lib, diagrams-rasterific
+, foldl, http-client, list-t, microlens, statistics, zip-archive
+, llvmPackages
+}:
+mkDerivation {
+  pname = "Frames";
+  version = "0.4.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base contravariant deepseq discrimination ghc-prim hashable pipes
+    pipes-bytestring pipes-group pipes-parse pipes-safe pipes-text
+    primitive readable template-haskell text transformers vector vinyl
+  ];
+  testHaskellDepends = [
+    base directory hspec htoml HUnit pipes pretty regex-applicative
+    template-haskell temporary text unordered-containers vinyl
+    Chart Chart-diagrams diagrams-lib diagrams-rasterific
+    foldl http-client list-t microlens statistics zip-archive
+    llvmPackages.llvm
+  ];
+  benchmarkHaskellDepends = [ base criterion pipes transformers llvmPackages.llvm ];
+  description = "Data frames For working with tabular data files";
+  license = stdenv.lib.licenses.bsd3;
+}
