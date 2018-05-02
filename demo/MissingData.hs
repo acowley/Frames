b8fc72eb7cca1e6ea47cb3692e217ccdf62caa29
Anthony Cowley <acowley@gmail.com>
2018-05-01 22:57:28 -0400

    vinyl-0.9 compatibility and mild `XRec` usage
    The need for type annotations is annoying, but I think/hope the types
    are more relevant than the value-level noise in the non-XRec version.

--- demo/MissingData.hs
+++ demo/MissingData.hs
@@ -6,10 +6,10 @@
 -- | An example of dealing with rows that contain missing data. We may
 -- want to fill in the gaps with default values.
 import Data.Monoid (First(..))
-import Data.Vinyl (Rec(..), ElField(..), rmap, rapply)
-import Data.Vinyl (XRec(..), IsoXRec(..))
-import Data.Vinyl.Functor (Compose(..), Lift(..), (:.))
+import Data.Vinyl (Rec(..), ElField(..), rapply, xrec, rmapX)
+import Data.Vinyl.Functor (Compose(..), (:.))
 import Data.Vinyl.Class.Method
+
 import Frames
 
 import Pipes (cat, Producer, (>->))
@@ -44,19 +44,16 @@ defFirst = def
 -- Real data often has holes. Here we have the 'MyString' column, but
 -- not the others.
 holyRow :: Rec (First :. ElField) '[MyString, MyInt, MyBool]
-holyRow = fromXRec $ pure ("joe") ::& mempty ::& mempty ::& XRNil
+holyRow = xrec (pure "joe", mempty, mempty)
 
-liftCompose2 :: (f (g a) -> h (j b) -> k (l c))
-             -> Compose f g a -> Compose h j b -> Compose k l c
-liftCompose2 f (Compose x) (Compose y) = Compose (f x y)
-
-mappendC :: Monoid (f (g x)) => Compose f g x -> Compose f g x -> Compose f g x
-mappendC = liftCompose2 mappend
+test :: Rec (First :. ElField) '[ "name" :-> String, "age" :-> Int ]
+test = xrec (pure "joe", mempty)
 
 -- We can fill in the holes with our default record.
 unholy :: Maybe (Record '[MyString, MyInt, MyBool])
-unholy = recMaybe . rmap (Compose . getFirst . getCompose)
-       $ rapply (rmap (Lift . flip mappendC) def) holyRow
+unholy = recMaybe . rmapX @(First :. ElField) getFirst
+       $ rapply (rmapX @(First :. ElField) (flip mappend) def) holyRow
+
 
 -- * Reading a CSV file with missing data
 
@@ -65,19 +62,14 @@ instance Default (ElField ("col_b" :-> Text)) where def = Field mempty
 
 tableTypes "Row" "data/missing.csv"
 
-recAla :: (forall a. f a -> g a) -> (forall a. g a -> f a) -> (Rec g rs -> Rec g rs) -> Rec f rs -> Rec f rs
-recAla tin tout f = rmap tout . f . rmap tin
-
-alaC :: (forall (t :: u). g t -> h t) -> (g :. f) a -> (h :. f) a
-alaC f (Compose x) = Compose (f x)
-
 -- | Fill in missing columns with a default 'Row' value synthesized
 -- from 'Default' instances.
 holesFilled :: MonadSafe m => Producer Row m ()
 holesFilled = readTableMaybe "data/missing.csv" >-> P.map (fromJust . holeFiller)
   where holeFiller :: Rec (Maybe :. ElField) (RecordColumns Row) -> Maybe Row
-        holeFiller = recMaybe . recAla (alaC First) (alaC getFirst)
-                                       (rapply (rmap (Lift . flip mappendC) def))
+        holeFiller = recMaybe . rmapX @(First :. ElField) getFirst
+                   . rapply (rmapX @(First :. ElField) (flip mappend) def)
+                   . rmapX @_ @(First :. ElField) First
         fromJust = maybe (error "Frames holesFilled failure") id
 
 showFilledHoles :: IO ()
