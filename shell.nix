a3760ffd3c54bc31f51e4cbaed57e55aa5cec100
Anthony Cowley <acowley@gmail.com>
2018-05-08 13:11:38 -0400

    Building Frames with a recent nixos-unstable nixpkgs checkout

--- shell.nix
+++ shell.nix
@@ -10,8 +10,13 @@ packageSet = pkgs.haskell.packages.${compiler};
     if withHoogle then
       packageSet.override {
         overrides = (self: super: {
-      ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ps.intero ]); };
-          vinyl = super.callPackage ~/Projects/Vinyl {};
+      ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ps.intero ps.cabal-install]); };
+          vinyl = super.callHackage "vinyl" "0.8.1.1" {};
+          pipes-group = pkgs.haskell.lib.dontCheck super.pipes-group;
+          diagrams-svg = pkgs.haskell.lib.doJailbreak super.diagrams-svg;
+          diagrams-postscript = pkgs.haskell.lib.doJailbreak super.diagrams-postscript;
+          diagrams-rasterific = pkgs.haskell.lib.doJailbreak super.diagrams-rasterific;
+          Chart-diagrams = pkgs.haskell.lib.doJailbreak super.Chart-diagrams;
           intero = pkgs.haskell.lib.dontCheck (super.callPackage ~/src/intero {});
           ghcWithPackages = self.ghc.withPackages;
         });
