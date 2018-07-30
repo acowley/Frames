65ab1f2834b433da2e0c598ad107309e4da46e37
Anthony Cowley <acowley@gmail.com>
2018-07-30 13:05:30 -0400

    Update nix package overrides
    Compatible with the `nixos-unstable` channel of nixpkgs in late July
    2018: fda46a645c962a7ae01680a0a0e290a6f0b5de1b

--- shell.nix
+++ shell.nix
@@ -12,12 +12,8 @@ packageSet = pkgs.haskell.packages.${compiler};
         overrides = (self: super: {
       ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ps.intero ps.cabal-install ]); };
           vinyl = super.callPackage ~/Projects/Vinyl {};
-          diagrams-core = super.callPackage ./diagrams-core.nix {};
-          diagrams-lib = pkgs.haskell.lib.doJailbreak super.diagrams-lib;
-          diagrams-svg = pkgs.haskell.lib.doJailbreak super.diagrams-svg;
-          diagrams-rasterific = pkgs.haskell.lib.doJailbreak super.diagrams-rasterific;
-          diagrams-postscript = pkgs.haskell.lib.doJailbreak super.diagrams-postscript;
-          pipes-group = pkgs.haskell.lib.dontCheck super.pipes-group;
+          Chart = super.callHackage "Chart" "1.9" {};
+          SVGFonts = super.callHackage "SVGFonts" "1.6.0.3" {};
           intero = pkgs.haskell.lib.dontCheck (super.callPackage ~/src/intero {});
           ghcWithPackages = self.ghc.withPackages;
         });
