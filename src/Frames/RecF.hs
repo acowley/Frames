a30aa1e36bf6de6fd53036a52962158acfe8a6de
Anthony Cowley <acowley@gmail.com>
2018-08-07 13:28:36 -0400

    Reinstate `mapMonoV` and `mapMono`

--- src/Frames/RecF.hs
+++ src/Frames/RecF.hs
@@ -20,6 +20,7 @@ module Frames.RecF (V.rappend, V.rtraverse, rdel, CanDelete,
                     -- pattern (:&), pattern Nil,
                     AllCols,
                     UnColumn, -- AsVinyl(..)
+                    StripFieldNames(..),
                     mapMono,
                     -- mapMethod,
                     -- runcurry, runcurry', runcurryA, runcurryA',
@@ -30,7 +31,8 @@ module Frames.RecF (V.rappend, V.rtraverse, rdel, CanDelete,
 -- import Data.List (intercalate)
 import Data.Proxy
 import qualified Data.Vinyl as V
-import Data.Vinyl (Rec(RNil), RecApplicative(rpure))
+import Data.Vinyl (Rec(..), RecApplicative(rpure))
+import Data.Vinyl.Derived (StripFieldNames(..), Unlabeled)
 import Data.Vinyl.Functor ((:.))
 import Data.Vinyl.TypeLevel
 import Frames.Col
@@ -93,102 +95,32 @@ type family UnColumn (ts :: [(Symbol,*)]) where
 -- | Enforce a constraint on the payload type of each column.
 type AllCols c ts = AllConstrained c (UnColumn ts)
 
--- | Remove the column name phantom types from a record, leaving you
--- with an unadorned Vinyl 'Rec'.
--- class AsVinyl f (ts :: [k]) where
---   toVinyl :: Rec f ts -> Rec f (UnColumn ts)
---   fromVinyl :: Rec f (UnColumn ts) -> Rec f ts
+-- | Map a function across a 'Rec' with monomorphic (i.e. all the
+-- same) indices.
+mapMonoV :: (AllAre a ts, Functor f)
+         => (a -> b)
+         -> Rec f ts
+         -> Rec f (ReplaceAll b ts)
+mapMonoV _ RNil = RNil
+mapMonoV f (x :& xs) = fmap f x :& mapMonoV f xs
 
--- instance AsVinyl '[] where
---   toVinyl _ = RNil
---   fromVinyl _ = RNil
-
--- instance (AsVinyl ts, KnownSymbol s) => AsVinyl (s :-> t ': ts) where
---   toVinyl (x V.:& xs) = fmap getCol x V.:& toVinyl xs
---   fromVinyl (x V.:& xs) = fmap Col x V.:& fromVinyl xs
--- #if __GLASGOW_HASKELL__ < 800
---   fromVinyl _ = error "GHC coverage checker isn't great"
--- #endif
-
--- | Map a function across a homogeneous, monomorphic 'Rec'.
-mapMono :: (Functor f, AllAre a ts)
-        => (a -> b) -> Rec f ts -> Rec f (ReplaceAll b ts)
-mapMono _ RNil = RNil
-mapMono f (x V.:& xs) = fmap f x V.:& mapMono f xs
-
--- | Map a function across a homogeneous, monomorphic 'Rec'.
--- mapMono :: (AllAre a (UnColumn ts), AsVinyl ts, Functor f,
---             AsVinyl (ReplaceColumns b ts),
---             ReplaceAll b (UnColumn ts) ~ UnColumn (ReplaceColumns b ts))
---         => (a -> b) -> Rec f ts -> Rec f (ReplaceColumns b ts)
--- mapMono f = fromVinyl . mapMonoV f . toVinyl
-
--- | Map a typeclass method across a 'Rec' each of whose fields
--- have instances of the typeclass.
--- mapMethodV :: forall c f ts. (Functor f, AllConstrained c ts)
---            => (forall a. c a => a -> a) -> Rec f ts -> Rec f ts
--- -- mapMethodV f = go
--- --   where go :: AllConstrained c ts' => Rec f ts' -> Rec f ts'
--- --         go RNil = RNil
--- --         go (x V.:& xs) = fmap f x V.:& go xs
--- mapMethodV = V.rmapMethod . fmap
-
--- | Map a typeclass method across a 'Rec' each of whose fields
--- has an instance of the typeclass.
--- mapMethod :: forall c f ts.
---              (Functor f, AllConstrained c (UnColumn ts), AsVinyl ts)
---           => (forall a. c a => a -> a) -> Rec f ts -> Rec f ts
--- mapMethod f = fromVinyl . mapMethodV @c f . toVinyl
-
--- * Currying Adapted from "Vinyl.Curry"
-
--- -- | N-ary version of 'uncurry' over functorial frame rows. See 'V.runcurry'.
--- runcurry :: (Functor f)
---          => V.CurriedF f (UnColumn ts) a -> Rec f ts -> a
--- runcurry = (. toVinyl) . V.runcurry
--- {-# INLINABLE runcurry #-}
-
--- -- | N-ary version of 'uncurry' over pure frame rows. See 'V.runcurry''.
--- runcurry' :: AsVinyl ts => V.Curried (UnColumn ts) a -> FieldRec ts -> a
--- runcurry' = (. toVinyl) . V.runcurry'
--- {-# INLINABLE runcurry' #-}
-
--- -- | Lift an N-ary function to work over a row of 'Applicative'
--- -- computations. See 'V.runcurryA'.
--- runcurryA' :: (Applicative f, AsVinyl ts)
---            => V.Curried (UnColumn ts) a -> Rec f ts -> f a
--- runcurryA' = (. toVinyl) . V.runcurryA'
-
--- -- | Lift an N-ary function over types in @g@ to work over a record of
--- -- 'Compose'd 'Applicative' computations. A more general version of
--- -- 'runcurryA''.
--- runcurryA :: (Applicative f, Functor g, AsVinyl ts)
---           => V.CurriedF g (UnColumn ts) a -> Rec (Compose f g) ts -> f a
--- runcurryA = (. toVinyl) . V.runcurryA
+-- | Map a function across a homogeneous 'Rec' of named values. This
+-- is a thin wrapper over 'mapMonoV'.
+mapMono :: (AllAre a (Unlabeled ts),
+            StripFieldNames ts,
+            StripFieldNames (ReplaceAllSnd b ts),
+            ReplaceAll b (Unlabeled ts) ~ Unlabeled (ReplaceAllSnd b ts))
+        => (a -> b)
+        -> Rec V.ElField ts
+        -> Rec V.ElField (ReplaceAllSnd b ts)
+mapMono f = withNames . mapMonoV f . stripNames
 
 -- | A constraint that a field can be deleted from a record.
 type CanDelete r rs = (V.RElem r rs (RIndex r rs), RDelete r rs V.⊆ rs)
 
 -- | Delete a field from a record
-rdel :: CanDelete r rs => proxy r -> Rec f rs -> Rec f (RDelete r rs)
-rdel _ = V.rcast
-
--- -- | The ability to pretty print a 'Rec''s fields.
--- class ShowRec f (rs :: [k]) where
---   showRec' :: Rec f rs -> [String]
-
--- instance Functor f => ShowRec f '[] where
---   showRec' _ = []
-
--- instance forall s f a (rs :: [(Symbol,*)]). (KnownSymbol s, Show (f (Col' s a)), ShowRec f rs)
---   => ShowRec f (s :-> a ': rs) where
---   showRec' (x :& xs) = show (col' <$> x :: f (Col' s a)) : showRec' xs
---   showRec' _ = error "GHC coverage error"
-
--- | Pretty printing of 'Rec' values.
--- showRec :: ShowRec f rs => Rec f rs -> String
--- showRec :: (Show (Rec f rs), Functor f) => Rec f rs -> String
--- showRec r = show (V.rmap (fmap Col') r)
+rdel :: CanDelete r rs => Rec f rs -> Rec f (RDelete r rs)
+rdel = V.rcast
 
 -- | Build a record whose elements are derived solely from a
 -- constraint satisfied by each.
