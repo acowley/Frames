1c40ab9eec16346c92a19b56411e417541a00992
Anthony Cowley <acowley@gmail.com>
2018-09-24 11:16:27 -0400

    GHC 8.6.1 compatibility; bump pipes-safe

--- shell.nix
+++ shell.nix
@@ -12,6 +12,12 @@ packageSet = pkgs.haskell.packages.${compiler};
         overrides = (self: super: {
         ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ps.intero ps.cabal-install ]); };
           vinyl = super.callPackage ~/Projects/Vinyl {};
+          pipes-safe = super.callCabal2nix "pipes-safe" (pkgs.fetchFromGitHub {
+            owner = "Gabriel439";
+            repo = "Haskell-Pipes-Safe-Library";
+            rev = "de71394ea29be8558575392e4ad66f98b3d8b5e5";
+            sha256 = "0i2l36xwqskhm5kcdvmyfy2n7cf2i2qdsrap5nib825gq2mnv9z5";
+          }) {};
           # Chart = super.callHackage "Chart" "1.9" {};
           SVGFonts = super.callHackage "SVGFonts" "1.6.0.3" {};
           intero = pkgs.haskell.lib.dontCheck (super.callPackage ~/src/intero {});
