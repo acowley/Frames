b42aacf6dc2453b6189f9ad4afc283c27ef2f4a4
Anthony Cowley <acowley@gmail.com>
2019-09-10 14:20:56 -0400

    Pull HIE into the development environment
    nixpkgs recently updated to Cabal 3.0.0.0 which does not work with
    HIE, so some contortions are needed to pull in cabal-install-2.4.1.0.

--- shell.nix
+++ shell.nix
@@ -4,13 +4,16 @@
 
 let
   pkgs = import <nixpkgs> {};
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+  hie = all-hies.selection { selector = p: { inherit (p) ghc865; };};
   f = import ./default.nix;
 packageSet = pkgs.haskell.packages.${compiler};
   hspkgs = (
     if withHoogle then
       packageSet.override {
         overrides = (self: super: {
-        ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ ps.cabal-install ]); };
+          ghc = super.ghc // { withPackages = f: super.ghc.withHoogle (ps: f ps ++ [ ]); };
+          # Cabal = super.Cabal_2_4_1_0;
           vinyl = pkgs.haskell.lib.dontBenchmark (super.callPackage ~/Projects/Vinyl {});
           # pipes-safe = super.callCabal2nix "pipes-safe" (pkgs.fetchFromGitHub {
           #   owner = "Gabriel439";
@@ -19,6 +22,12 @@ packageSet = pkgs.haskell.packages.${compiler};
           #   sha256 = "0i2l36xwqskhm5kcdvmyfy2n7cf2i2qdsrap5nib825gq2mnv9z5";
           # }) {};
           # Chart = super.callHackage "Chart" "1.9" {};
+          cabal-install-2_4 = super.callHackage "cabal-install" "2.4.1.0" {
+            Cabal = super.Cabal_2_4_1_0;
+          };
+          hackage-security = pkgs.haskell.lib.dontCheck (super.callHackage "hackage-security" "0.5.3.0" {
+            Cabal = super.Cabal_2_4_1_0;
+          });
           SVGFonts = pkgs.haskell.lib.doJailbreak (super.callHackage "SVGFonts" "1.6.0.3" {});
           # intero = pkgs.haskell.lib.dontCheck (super.callPackage ~/src/intero {});
           Chart = super.callCabal2nix "Chart" (pkgs.fetchFromGitHub {
@@ -40,5 +49,18 @@ packageSet = pkgs.haskell.packages.${compiler};
       else packageSet
   );
   drv = hspkgs.callPackage f {};
+  ghc = hspkgs.ghc.withHoogle (_: drv.passthru.getBuildInputs.haskellBuildInputs);
 in
-  if pkgs.lib.inNixShell then drv.env else drv
+# if pkgs.lib.inNixShell then drv.env else drv
+pkgs.mkShell {
+  buildInputs = [ hie # hspkgs.cabal-install
+                  hspkgs.cabal-install-2_4
+                  ghc ];
+  shellHook = ''
+    export NIX_GHC='${ghc}/bin/ghc'
+    export NIX_GHCPKG='${ghc}/bin/ghc-pkg'
+    export NIX_GHC_DOCDIR='${drv.compiler.doc}/share/doc/ghc/html'
+    export NIX_GHC_LIBDIR='${ghc}/lib/${drv.compiler.name}'
+    export HIE_HOOGLE_DATABASE='${ghc}/share/doc/hoogle/default.hoo'
+  '';
+}
