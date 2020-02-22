ecdd7410532c7e9aed3c00dcbc31619a33bc255a
Anthony Cowley <acowley@gmail.com>
2020-02-22 11:14:33 -0500

    Changed relative data file paths to work from package root

--- demo/framestack/app/Main.hs
+++ demo/framestack/app/Main.hs
@@ -7,31 +7,31 @@ import Lens.Micro
 import qualified Pipes as P
 import qualified Pipes.Prelude as P
 
-tableTypes "Row" "../../data/data1.csv"
+tableTypes "Row" "data/data1.csv"
 
 tbl :: IO [Row]
-tbl = runSafeT . P.toListM $ readTable "../../data/data1.csv"
+tbl = runSafeT . P.toListM $ readTable "data/data1.csv"
 
 
 ageDoubler :: (Age ∈ rs) => Record rs -> Record rs
 ageDoubler = age %~ (* 2)
 
 tbl2 :: IO [Row]
-tbl2 = runSafeT . P.toListM $ readTable "../../data/data2.csv"
+tbl2 = runSafeT . P.toListM $ readTable "data/data2.csv"
 
 tbl2a :: IO [ColFun Maybe Row]
-tbl2a = runSafeT . P.toListM $ readTableMaybe "../../data/data2.csv"
+tbl2a = runSafeT . P.toListM $ readTableMaybe "data/data2.csv"
 
 -- Sample data from http://support.spatialkey.com/spatialkey-sample-csv-data/
 -- Note: We have to replace carriage returns (\r) with line feed
 -- characters (\n) for the text library's line parsing to work.
-tableTypes "Ins" "../../data/FL2.csv"
+tableTypes "Ins" "data/FL2.csv"
 
 insuranceTbl :: MonadSafe m => P.Producer Ins m ()
-insuranceTbl = readTable "../../data/FL2.csv"
+insuranceTbl = readTable "data/FL2.csv"
 
 insMaybe :: MonadSafe m => P.Producer (ColFun Maybe Ins) m ()
-insMaybe = readTableMaybe "../../data/FL2.csv"
+insMaybe = readTableMaybe "data/FL2.csv"
 
 type TinyIns = Record [PolicyID, PointLatitude, PointLongitude]
 
