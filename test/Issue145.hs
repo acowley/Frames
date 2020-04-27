2f2205d2d91eeec906bce9c2c25c6c63e75077a4
Anthony Cowley <acowley@gmail.com>
2020-04-27 12:16:31 -0400

    Add a TypeApplications pragma to a spec test

--- test/Issue145.hs
+++ test/Issue145.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Issue145 where
