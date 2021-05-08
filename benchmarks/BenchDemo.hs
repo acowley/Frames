56da93f3943361642223605d27d8affe8a19bf58
Anthony Cowley <acowley@gmail.com>
2021-05-08 01:39:28 -0400

    Use a larger column type inference prefix in the benchdemo program
    It was pointed out by Eric Lindblad that the benchdemo output was
    similar but not identical to the comparison pandas output. This was
    due to an incorrect type inference of a column of the data file used
    as the first 1000 lines of the file are insufficient to observe the
    variety of values that column should be able to represent.

--- benchmarks/BenchDemo.hs
+++ benchmarks/BenchDemo.hs
@@ -10,9 +10,16 @@
 -- @$ $(which time) -v dist-newstyle/build/x86_64-linux/ghc-8.8.2/Frames-0.6.2/x/benchdemo/build/benchdemo/benchdemo@
 import qualified Control.Foldl as F
 import Frames
+import Frames.TH (rowGen, RowGen (rowTypeName, inferencePrefix))
 import Pipes.Prelude (fold)
 
-tableTypes "Ins" "data/FL2.csv"
+-- The simple use of 'tableTypes' commented out here is what one
+-- typically uses; it infers column types based the first 1000 rows of
+-- the data file. In this data file, however, we need to see more rows
+-- to properly identify the types.
+
+-- tableTypes "Ins" "data/FL2.csv"
+tableTypes' (rowGen "data/FL2.csv") { rowTypeName = "Ins", inferencePrefix = 2500 }
 
 main :: IO ()
 main = do (lat,lng,n) <- runSafeT $ F.purely fold f (readTable "data/FL2.csv")
