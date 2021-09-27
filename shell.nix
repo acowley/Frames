57129fdba2e4e1dbed441bbf82f7693e2c7f2c5a
Matthieu Coudron <teto@users.noreply.github.com>
2021-09-27 22:56:31 +0200

    nix: added flake-compat (#161)
    The flake-compat project makes it possible to run a flake.nix with
    legacy-nix via the nix-shell command.
    Since this serves the same purpose as niv, but will be a future builtin
    of nix, I removed niv.

--- shell.nix
+++ shell.nix
@@ -1,46 +1,9 @@
-{ compiler ? null
-, withHoogle ? true
-, sources ? import ./nix/sources.nix
-, nixConfig ? { allowBroken = true; }
-, pkgs ? import sources.nixpkgs-chan { config = nixConfig; }
-}:
-let
-  overrideByVersion = if compiler == "ghc8101"
-                      then self: super: { }
-                      else self: super: { };
-
-  hspkgs = (if isNull compiler 
-            then pkgs.haskellPackages
-            else pkgs.haskell.packages.${compiler}).override {
-    overrides = self: super: {
-      statestack = pkgs.haskell.lib.doJailbreak super.statestack;
-      svg-builder = pkgs.haskell.lib.doJailbreak super.svg-builder;
-      size-based = pkgs.haskell.lib.doJailbreak super.size-based;
-      monoid-extras = pkgs.haskell.lib.doJailbreak super.monoid-extras;
-      active = pkgs.haskell.lib.doJailbreak super.active;
-      dual-tree = pkgs.haskell.lib.doJailbreak super.dual-tree;
-      diagrams-core = pkgs.haskell.lib.doJailbreak super.diagrams-core;
-      diagrams-lib = pkgs.haskell.lib.doJailbreak super.diagrams-lib;
-      diagrams-postscript = pkgs.haskell.lib.doJailbreak super.diagrams-postscript;
-      SVGFonts = pkgs.haskell.lib.doJailbreak super.SVGFonts;
-      diagrams-svg = pkgs.haskell.lib.doJailbreak super.diagrams-svg;
-      diagrams-rasterific = pkgs.haskell.lib.doJailbreak super.diagrams-rasterific;
-    } // overrideByVersion self super;
-  };
-  drv = hspkgs.callPackage ./default.nix {};
-  ghc = hspkgs.ghc.withHoogle (ps: drv.passthru.getBuildInputs.haskellBuildInputs);
-in
-pkgs.mkShell {
-  buildInputs = [ ghc
-                  hspkgs.haskell-language-server
-                  hspkgs.cabal-install
-                  # pkgs.llvmPackages_7.llvm
-                  pkgs.llvmPackages_latest.llvm
-                ]
-  # hspkgs.ghcide
-  ;
-  # shellHook = ''
-  #   source <(grep '^export NIX_' ${ghc}/bin/ghc)
-  #   source <(echo 'export HIE_HOOGLE_DATABASE='$(grep -F -- '--database' ${ghc}/bin/hoogle | sed 's/.* --database \(.*\.hoo\).*/\1/'))
-  # '';
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash; }
+) {
+  src =  ./.;
+}).shellNix
